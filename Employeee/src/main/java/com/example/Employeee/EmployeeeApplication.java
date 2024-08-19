108313252058e7d6e82f2ef692a8020db8a6d877
Malavika Dara <mdara@nisum.com>
2024-08-19 16:36:35 +0530

    Employee

---
+++ Employeee/src/main/java/com/example/Employeee/EmployeeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employeee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeeApplication.class, args);
+	}
+
+}
